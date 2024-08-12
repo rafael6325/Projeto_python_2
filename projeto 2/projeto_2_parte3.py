51805f6d65ff729b56ec4c448b9803a475984262
rafael6325 <rafael6325@outlook.com.br>
2024-08-12 17:28:03 -0300

    Add files via upload

---
+++ projeto 2/projeto_2_parte3.py
@@ -0,0 +1,4 @@
+for numero in range(10, -1, -1):
+    print(numero);
+
+print("Contagem regressiva concluída!");
---
+++ projeto 2/projeto_2_parte6.py
@@ -0,0 +1,5 @@
+limite = int(input("Digite um número inteiro: "));
+
+for numero in range(1, limite + 1):
+    if numero % 2 != 0:
+        print(numero);
